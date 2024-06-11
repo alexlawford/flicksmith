650a4e332b1d084db4c30656267bee03bc2450fe
Alexander Browne <alexlawford@icloud.com>
2024-06-11 14:56:40 +0100

    thanks page padding

--- src/routes/submitted/+page.svelte
+++ src/routes/submitted/+page.svelte
@@ -3,10 +3,11 @@ import Header from '$lib/Header.svelte'
 
 </script>
 <Header bg='bg-base-300' />
-<div class="relative flex items-center justify-center h-screen overflow-hidden">
-    <div class="max-w-md relative z-3 -mt-20">
+<div class="relative h-screen">
+    <div class="m-auto max-w-xl mt-12 p-8">
         <h1 class="mb-5 text-4xl font-bold text-center">Thanks.</h1>
         <p class="mb-5">Check your inbox for a confirmation email.</p>
         <p>You'll be hearing from some <strong>great directors</strong> very soon!</p>
     </div>
 </div>
+
